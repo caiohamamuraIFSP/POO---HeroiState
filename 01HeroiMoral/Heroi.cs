288bf954d5181052063d1893ba76c6d1b2ba1af8
Caio Hamamura <hamamura.caio@ifsp.edu.br>
2022-05-04 20:45:50 -0300

    Padrao state

--- 01HeroiMoral/Heroi.cs
+++ 01HeroiMoral/Heroi.cs
@@ -1,47 +1,123 @@
-public enum Moral 
+public interface IMoral
 {
-    Alta,
-    Normal,
-    Baixa
-};
+    public void Ataca();
+    public void Nadar();
+    public void Defender();
+    public void AbaixarMoral(Heroi heroi);
+    public void ElevarMoral(Heroi heroi);
+}
 
+public class MoralAlta : IMoral
+{
+    public void AbaixarMoral(Heroi heroi)
+    {
+        heroi.moral = new MoralMedia();
+    }
+
+    public void Ataca()
+    {
+        System.Console.WriteLine("Seu ataque é super efetivo!");
+    }
+
+    public void Defender()
+    {
+        System.Console.WriteLine("Você desvia de todos os ataques como um ninja!");
+    }
+
+    public void ElevarMoral(Heroi heroi)
+    {
+    }
+
+    public void Nadar()
+    {
+        System.Console.WriteLine("Você nada super rápido!");
+    }
+}
+
+public class MoralMedia : IMoral
+{
+    public void AbaixarMoral(Heroi heroi)
+    {
+        heroi.moral = new MoralBaixa();
+    }
+    public void Ataca()
+    {
+        System.Console.WriteLine("Você atacou!");
+    }
+
+    public void Defender()
+    {
+        System.Console.WriteLine("Você consegue aparar alguns ataques!");
+    }
+
+    public void ElevarMoral(Heroi heroi)
+    {
+        heroi.moral = new MoralAlta();
+    }
+
+    public void Nadar()
+    {
+        System.Console.WriteLine("Você está nadando!");
+    }
+}
+
+public class MoralBaixa : IMoral
+{
+    public void AbaixarMoral(Heroi heroi)
+    {
+    }
+    public void Ataca()
+    {
+        System.Console.WriteLine("Você está fraco para atacar!");
+    }
+
+    public void Defender()
+    {
+        System.Console.WriteLine("Você tomou todos os ataques!");
+    }
+
+    public void ElevarMoral(Heroi heroi)
+    {
+        heroi.moral = new MoralMedia();
+    }
+
+    public void Nadar()
+    {
+        System.Console.WriteLine("Você se afogou!");
+    }
+}
 
 
 public class Heroi
 {
-    Moral moral = Moral.Normal;
-    public Heroi(Moral moral) 
+    public IMoral moral = new MoralMedia();
+    public Heroi(IMoral moral) 
     {
         this.moral = moral;
     }
 
     public void Ataca() 
     {
-        if (moral == Moral.Alta) 
-            System.Console.WriteLine("Seu ataque é super efetivo!");
-        else if (moral == Moral.Normal)
-            System.Console.WriteLine("Você atacou!");
-        else
-            System.Console.WriteLine("Você está fraco para atacar!");
+        moral.Ataca();
     }
 
     public void Defender() 
     {
-        if (moral == Moral.Alta) 
-            System.Console.WriteLine("Você desvia de todos os ataques como um ninja!");
-        else if (moral == Moral.Normal)
-            System.Console.WriteLine("Você consegue aparar alguns ataques!");
-        else
-            System.Console.WriteLine("Você tomou todos os ataques!");
+        moral.Defender();
     }
 
     public void Nadar() 
     {
-        if (moral == Moral.Alta) 
-            System.Console.WriteLine("Você nada super rápido!");
-        else if (moral == Moral.Normal)
-            System.Console.WriteLine("Você está nadando!");
-        else
-            System.Console.WriteLine("Você se afogou!");
+        moral.Nadar();
+    }
+
+    public void AbaixarMoral()
+    {
+        moral.AbaixarMoral(this);
+    }
+
+    public void ElevarMoral()
+    {
+        moral.ElevarMoral(this);
     }
 }
--- 01HeroiMoral/Program.cs
+++ 01HeroiMoral/Program.cs
@@ -6,7 +6,34 @@ namespace _01HeroiMoral
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var heroi = new Heroi(new MoralAlta());
+
+            heroi.Ataca();
+            heroi.Defender();
+            
+            Console.WriteLine();
+            Console.WriteLine("Abaixando moral");
+            
+            heroi.AbaixarMoral();
+            heroi.Ataca();
+            heroi.Defender();
+
+            Console.WriteLine();
+            Console.WriteLine("Abaixando moral");
+            
+            heroi.AbaixarMoral();
+            heroi.Ataca();
+            heroi.Defender();
+
+            Console.WriteLine();
+            Console.WriteLine("Elevar moral 2x");
+            
+            heroi.ElevarMoral();
+            heroi.ElevarMoral();
+            heroi.Ataca();
+            heroi.Defender();
+
+            
         }
     }
 }
